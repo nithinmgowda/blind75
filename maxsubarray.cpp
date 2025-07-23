95e6ec769c4fd5072a4b67acf1a083b6df717d7c
Nithin.M.Gowda <149388022+nithinmgowda@users.noreply.github.com>
2025-07-23 17:29:37 +0530

    1.3

---
+++ maxsubarray.cpp
@@ -0,0 +1,18 @@
+class solution{
+    public:
+
+    int maxsubarray(vector<int>& nums){
+        int n = nums.size();
+        int current_max = nums[0];
+        int global_max = nums[0];
+
+        for(int i=1; i<n; i++){
+            current_max = max(nums[i] + current_max, nums[i]);
+            if(current_max>global_max){
+                global_max = current_max;
+
+            }
+        }
+        return golbal_max;
+    }
+    };
