c6df70cdd897b95525f6f95e11fc3aaecebce5b3
Nithin.M.Gowda <149388022+nithinmgowda@users.noreply.github.com>
2025-07-23 15:04:38 +0530

    1.1

--- sorting/mergesort.cpp
+++ sorting/mergesort.cpp
@@ -2,14 +2,14 @@ void merge(int arr[], int l, int m, int r) {
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    // Create temp arrays
+    
     int L[n1], R[n2];
 
-    // Copy data to temp arrays
+   
     for (int i = 0; i < n1; i++) L[i] = arr[l + i];
     for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
 
-    // Merge the temp arrays
+    
     int i = 0, j = 0, k = l;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j])
@@ -18,7 +18,7 @@ void merge(int arr[], int l, int m, int r) {
             arr[k++] = R[j++];
     }
 
-    // Copy remaining elements
+    
     while (i < n1) arr[k++] = L[i++];
     while (j < n2) arr[k++] = R[j++];
 }
