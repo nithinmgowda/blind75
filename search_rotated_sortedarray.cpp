680cb18ec139f31b02f8f335d993e990d6f27e8a
Nithin.M.Gowda <149388022+nithinmgowda@users.noreply.github.com>
2025-07-25 18:24:31 +0530

    1.6

---
+++ search_rotated_sortedarray.cpp
@@ -0,0 +1,32 @@
+//similar to find minimum in rotated sorted array
+//here the conditional statements bore u out..
+
+class solution{
+    public:
+
+    int search(vector<int>& nums, int target){
+        int n = nums.size(), left = 0, right = n-1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            
+            if(nums[mid] == target) {return mid;}
+            else if(nums[mid] >= nums[left]){
+                if(nums[left] <= target && target < nums[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+                }
+                else{
+                    if(nums[mid]< target && target <= nums[right]){
+                        left = mid + 1;
+
+                    }else{
+                        right = mid - 1;
+                    }
+                }
+            }
+            return -1;
+
+        }
+    };
