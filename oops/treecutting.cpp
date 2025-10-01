3bf4d4bd9134a4631b477fb2cca230be868e513f
Nithin.M.Gowda <149388022+nithinmgowda@users.noreply.github.com>
2025-10-01 21:40:57 +0530

    treecutting.cpp updated

--- oops/treecutting.cpp
+++ oops/treecutting.cpp
@@ -87,5 +87,92 @@ class Weapon{
 
 class Tree{
     string name;
-    
+    float width;
+    float height;
+    int health;
+    int wood_yield;
+    int Durability;
+    const int max_durability = 100;
+    const int min_durability = 0;
+    const int max_health = 1000;
+    const int min_health = 0;
+
+
+     public:
+
+    // status method to show the status of the tree
+
+     void showTreeStatus(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Width: "<<width<<endl;
+        cout<<"Height: "<<height<<endl;
+        cout<<"Health: "<<health<<endl;
+        cout<<"Wood Yield: "<<wood_yield<<endl;
+        cout<<"Is Alive: "<<isAlive()<<endl;
+    }
+
+    //parameterized constructor
+
+    Tree(string name, float width, float height, int health ,int wood_yield){
+        this -> name = name;
+        this -> width = width;
+        this -> height = height;
+        this -> health = health;
+        this -> wood_yield = wood_yield;
+    }
+
+    // getter methods
+
+   string getName(){return name;}
+   int getHealth(){return health;}
+   float getWidth(){return width;}
+   float getHeight(){return height;}
+   int getWood_yield(){return wood_yield;}
+
+
+    void setHeight(float h){
+        if(h >0){
+            this -> height = h;
+
+        }else{
+            cout<<"Invalid value"<<endl;
+        }
+    }
+    void setwidth(float w){
+        if(w > 0){
+            this -> width = w;
+            
+        }else{
+            cout<<"Invalid value"<<endl;
+        }
+     }
+
+    void sethealth(int h){
+        if(h >= min_health && h <= max_health){
+            this -> health = h;
+        }else{
+            cout<<"Invaild value"<<endl;
+        }
+     }
+
+
+     bool isAlive(){
+        if(health <= min_health)
+            return false;
+            else 
+            return true;
+     }
+
+    void takeDamage(int dmg){
+        health = max(min_health, health - dmg);
+        if(health == min_health)
+            cout<<name<<" has been cut down!"<<endl;
+     }
+
+     int Wood_Yield(){
+        float loss_factor = rand() % 21 + 75; // Random loss factor between 75 and 95
+        int yield = (height * width) * (loss_factor / 100.0);
+        return yield;
+     }
+
 }
